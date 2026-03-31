5c0d8f6e391780542e042c7d9e694e240079f6e2
syauqiachmadzen <syuqiachmadzen@gmail.com>
2026-03-31 22:38:46 +0700

    commit 4

--- TugasLooping_o133.cpp
+++ TugasLooping_o133.cpp
@@ -7,3 +7,9 @@ int n, pilihan;
 // Function cek bilangan prima
 bool isPrima(int n) {
     if (n <= 1) return false;
+
+    int i = 2;
+    while (i <= n / 2) {
+        if (n % i == 0) {
+            return false;
+        }
