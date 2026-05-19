8564e7d2f8ebdf2a46225ecbcf12bda393db095c
syauqiachmadzen-hub <syauqiachmadzen@gmail.com>
2026-05-19 18:10:22 +0700

    multiwaris 2

--- multiwaris.cpp
+++ multiwaris.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 using namespace std;
+
+class orang {
+public:
+    int umur;
+    orang(int pumur) :
+    umur(pumur)
+    
+    {
+        cout << "orang dibuat dengan umur" << "\n" << endl;
+    }
+};
+
